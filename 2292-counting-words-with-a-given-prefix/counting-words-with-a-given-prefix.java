bab257399e468124c0234a88a77a938e770bc375
Bhawya2531 <bhawya055@gmail.com>
2026-01-01 19:50:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (24.50%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,14 +1,12 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        return solve(words, pref, 0);
-    }
-    private int solve(String[] words, String pref, int i){
-        if(i==words.length){
-            return 0;
+        int count=0;
+    for(int i=0;i<words.length;i++){
+        if(words[i].startsWith(pref)){
+            count++;
         }
-        int count=words[i].startsWith(pref) ? 1:0;
-        return count+solve(words,pref,i+1);
+
+    }
+    return count;
     }
 }
-
-      
