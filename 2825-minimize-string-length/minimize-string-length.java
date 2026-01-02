03b9f99ebc0b4efcc161f5fcf62c22544bdceb23
Bhawya2531 <bhawya055@gmail.com>
2026-01-02 05:42:30 +0530

    Time: 4 ms (64.91%) | Memory: 46.6 MB (70.33%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.java
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -1,9 +1,19 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        Set<Character> unique = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            unique.add(c);
+        boolean[]seen=new boolean[26];
+        return solve(s,0,seen);
+
+    }
+    private int solve(String s, int index, boolean[]seen){
+        if(index==s.length()){
+            return 0;
         }
-        return unique.size();
+        char ch=s.charAt(index);
+        int idx=ch-'a';
+        if(seen[idx]){
+            return solve(s,index+1,seen);
+        }
+        seen[idx]=true;
+        return 1+solve(s,index+1,seen);
     }
 }
