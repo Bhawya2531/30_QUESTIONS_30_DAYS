551c9cc699ff8725d5f51e4b010e26d4c94d74f0
Bhawya2531 <bhawya055@gmail.com>
2026-01-02 12:34:24 +0530

    Time: 4 ms (19.93%) | Memory: 44.2 MB (7.38%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,19 +1,21 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-       return helper(word1, word2, 0);
+     return helper(word1, word2, 0);
     }
     private String helper(String w1, String w2, int i){
-        //Base case: Both String Exhausted
+        //base case : both strings exhausted 
         if(i>=w1.length() && i>=w2.length()){
             return "";
         }
         String result="";
-        if(i<w1.length()){
+        if(i < w1.length()){
             result+=w1.charAt(i);
         }
         if(i<w2.length()){
             result+=w2.charAt(i);
         }
-        return result+helper(w1,w2,i+1);
+        return result+ helper(w1,w2,i+1);
     }
 }
+
+   
