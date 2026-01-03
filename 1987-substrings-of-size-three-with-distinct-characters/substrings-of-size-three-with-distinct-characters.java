0abfae10375ddd835781106ff9ac1197df801543
Bhawya2531 <bhawya055@gmail.com>
2026-01-03 16:52:32 +0530

    Time: 1 ms (95.34%) | Memory: 42.6 MB (90.36%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int count=0;
+        int left=0;
+        for(int right=0;right<s.length();right++){
+            if(right-left+1==3){
+                char a=s.charAt(left);
+                char b=s.charAt(left+1);
+                char c=s.charAt(right);
+
+                if(a!=b && b!=c && c!=a){
+                    count++;
+                }
+left++;
+            }
+        }
+        return count;
+    }
+}
+            
