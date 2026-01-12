a9a798d796916654fcf0a3044afe2803fdb33607
Bhawya2531 <bhawya055@gmail.com>
2026-01-12 20:14:33 +0530

    Time: 7 ms (56.93%) | Memory: 46.8 MB (49.06%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -2,21 +2,32 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int n=mat.length;
         int m=mat[0].length;
-        int [][]ans=new int[n][m];
+        int [][]pref=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                int sum=0;
-                for(int r=Math.max(i-k,0);r<=Math.min(n-1,i+k);r++){
-                      for(int c=Math.max(j-k,0);c<=Math.min(m-1,j+k);c++){
-                        sum+=mat[r][c];
-                      }
-                }
+                pref[i][j]=mat[i][j];
+
+                if(i>0) pref[i][j]+=pref[i-1][j];
+                if(j>0) pref[i][j]+=pref[i][j-1];
+                if(i>0 && j>0) pref[i][j]-=pref[i-1][j-1];
+            }
+        }
+        int[][]ans= new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int top=Math.max(0,i-k);
+                int bottom=Math.min(n-1,i+k);
+                int left=Math.max(0,j-k);
+                int right=Math.min(m-1,j+k);
+
+                int sum=pref[bottom][right];
+                if(top>0) sum-=pref[top-1][right];
+                if(left>0) sum-=pref[bottom][left-1];
+                if(top>0 && left>0) sum+=pref[top-1][left-1];
+
                 ans[i][j]=sum;
             }
         }
         return ans;
     }
 }
-
-
-        
