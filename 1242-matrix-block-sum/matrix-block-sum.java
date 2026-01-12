cd6669a0606127ab9dbeecefee5f3cab2e5003c5
Bhawya2531 <bhawya055@gmail.com>
2026-01-12 16:05:00 +0530

    Time: 96 ms (13.67%) | Memory: 47 MB (12.73%) - LeetSync

---
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int [][]ans=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int sum=0;
+                for(int r=Math.max(i-k,0);r<=Math.min(n-1,i+k);r++){
+                      for(int c=Math.max(j-k,0);c<=Math.min(m-1,j+k);c++){
+                        sum+=mat[r][c];
+                      }
+                }
+                ans[i][j]=sum;
+            }
+        }
+        return ans;
+    }
+}
+
+
+        
