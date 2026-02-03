622c5b454ab547b56a3b32f2b16008c157e1a788
Bhawya2531 <bhawya055@gmail.com>
2026-02-03 22:27:33 +0530

    Time: 1 ms (72.27%) | Memory: 45 MB (10.08%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        int i = 1;
+
+        // Phase 1: strictly increasing
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
+        }
+
+        // Must have at least one element in the first segment
+        if (i == 1 || i == n) return false;
+
+        // Phase 2: strictly decreasing
+        while (i < n && nums[i] < nums[i - 1]) {
+            i++;
+        }
+
+        // Must have at least one element in the second segment
+        if (i == n) return false;
+
+        // Phase 3: strictly increasing
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
+        }
+
+        // If we traversed all elements, it's trionic
+        return i == n;
+    }
+}
+
+    
