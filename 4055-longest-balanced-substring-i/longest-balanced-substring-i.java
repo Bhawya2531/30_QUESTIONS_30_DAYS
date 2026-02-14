b103a53e65a1ebe072af366136e22101dfa6c64d
Bhawya2531 <bhawya055@gmail.com>
2026-02-15 00:53:01 +0530

    Time: 1504 ms (5.00%) | Memory: 47 MB (43.13%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -1,24 +1,22 @@
 class Solution {
     public int longestBalanced(String s) {
-        int n=s.length();
         int maxlen=0;
-        for(int i=0;i<n;i++){
-            int[]freq=new int[26];
-        for(int j=i;j<n;j++){
-            freq[s.charAt(j)-'a']++;
-            HashMap<Integer,Integer>map=new HashMap<>();
-            for(int k=0;k<26;k++){
-                if(freq[k]>0){
-                    map.put(freq[k],map.getOrDefault(freq[k],0)+1);
-                }
-            }
-            if(map.size()==1){
-                maxlen=Math.max(maxlen,j-i+1);
+      for(int i=0;i<s.length();i++){
+       int[]freq=new int[26];
+      for(int j=i;j<s.length();j++){
+        freq[s.charAt(j)-'a']++;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int k=0;k<26;k++){
+            if(freq[k]>0){
+                map.put(freq[k],map.getOrDefault(freq[k],0)+1);
             }
         }
+        if(map.size()==1){
+            maxlen=Math.max(maxlen,j-i+1);
         }
-        return maxlen;
+      }
+      }
+      return maxlen;
+
     }
 }
-
-                
