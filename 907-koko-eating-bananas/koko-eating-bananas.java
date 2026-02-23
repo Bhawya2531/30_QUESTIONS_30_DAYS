94f39e95f5005ca00649eefe2160bb97ebf55129
Bhawya2531 <bhawya055@gmail.com>
2026-02-23 11:21:42 +0530

    Time: 12 ms (55.99%) | Memory: 47.9 MB (73.67%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,31 @@
+class Solution {
+    boolean canEatAll(int[]piles, int mid, int h){
+        int actualhours=0;
+        for(int x:piles){
+            actualhours+=x/mid;
+            if(x%mid!=0) {
+                actualhours++;
+            }
+        }
+        return actualhours<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int left=1;
+        int right=0;
+            // find max pile
+        for(int x : piles){
+            right = Math.max(right, x);
+        }
+
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(canEatAll(piles,mid,h)){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+            }
+            return left;
+        }
+    }
