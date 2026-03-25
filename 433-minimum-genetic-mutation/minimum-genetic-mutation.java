8392abb58f5b1aa986dce9a7165e8afb4b412884
Bhawya2531 <bhawya055@gmail.com>
2026-03-25 13:31:05 +0530

    Time: 1 ms (94.28%) | Memory: 43 MB (38.29%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,45 +1,41 @@
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        HashSet<String> bankSet = new HashSet<>();
-        for(String b: bank) bankSet.add(b);
-
+        HashSet<String>bankSet=new HashSet<>();
+        for(String b:bank) bankSet.add(b);
         if(!bankSet.contains(endGene)) return -1;
-
-        HashSet<String> visited = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-
-        queue.offer(startGene);
+        HashSet<String>visited=new HashSet<>();
+        Queue<String>q=new LinkedList<>();
+        q.offer(startGene);
         visited.add(startGene);
+        char[]genes={'A','G','C','T'};
+        int level=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+        for(int i=0;i<size;i++){
+            String curr=q.poll();
+        if(curr.equals(endGene)) return level;
+        for(int j=0;j<curr.length();j++){
+            for(char ch:genes){
+                if(curr.charAt(j)==ch) continue;
+        StringBuilder sb=new StringBuilder(curr);
+        sb.setCharAt(j,ch);
+        String next=sb.toString();
 
-        char[] genes = {'A','C','G','T'};
-        int level = 0;
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            while(size-- > 0){
-                String curr = queue.poll();
-                if(curr.equals(endGene)) return level;
-
-                for(int i = 0; i < curr.length(); i++){
-                    for(char ch : genes){
-                        if(curr.charAt(i) == ch) continue;
-
-                        StringBuilder sb = new StringBuilder(curr);
-                        sb.setCharAt(i, ch);
-                        String next = sb.toString();
-
-                        if(bankSet.contains(next) && !visited.contains(next)){
-                            visited.add(next);
-                            queue.offer(next);
-                        }
-                    }
-                }
-            }
-            level++;
-        }
-        return -1;
+        if(bankSet.contains(next) && !visited.contains(next)){
+            visited.add(next);
+            q.offer(next);
+           }
+       }
+   }
+}
+level++;
+}
+return -1;
     }
 }
+
+
+
 
 
         
