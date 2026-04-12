7d568d8e95d4db0860a538febdea87e0f4222cdf
Bhawya2531 <bhawya055@gmail.com>
2026-04-12 17:03:51 +0530

    Time: 19 ms (26.50%) | Memory: 46.9 MB (15.53%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,35 +1,23 @@
 class Solution {
     public int countSubstrings(String s) {
         int n=s.length();
-        int[][]t=new int[n][n];
-        //initialize dp array with -1
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                t[i][j]=-1;
-            }
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(check(s,i,j,t)){
-                    count++;
+       boolean[][]dp=new boolean[n][n];
+       int count=0;
+       for(int L=1;L<=n;L++){
+        for(int i=0;i<=n-L;i++){
+            int j=i+L-1;
+            if(s.charAt(i)==s.charAt(j)){
+                if(L<=2){
+                    dp[i][j]=true;
+                }else{
+                    dp[i][j]=dp[i+1][j-1];
                 }
+                if(dp[i][j]) count++;
             }
         }
-    return count;
-    }
-    boolean check(String s, int i, int j, int[][]t){
-        if(i>=j) return true;
-        if(t[i][j]!=-1){
-            return t[i][j]==1;
-        }
-        if(s.charAt(i)!=s.charAt(j)){
-            t[i][j]=0;
-            return false;
-        }
-        boolean res=check(s,i+1,j-1,t);
-        t[i][j]=res? 1:0;
-        return res;
+       }
+       return count;
     }
 }
-       
+
+               
