91cee33dcb3139493bf1aad8c6a01c6d813919df
Bhawya2531 <bhawya055@gmail.com>
2026-04-19 12:38:06 +0530

    Time: 7 ms (56.83%) | Memory: 46.2 MB (47.61%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,21 +2,22 @@ class Solution {
     //TC=0(N^2)
     public boolean wordBreak(String s, List<String> wordDict) {
        Set<String>dict=new HashSet<>(wordDict);
-       Boolean[]dp=new Boolean[s.length()]; //Store repeating cases
+       Boolean[]dp=new Boolean[s.length()]; //Stores result of subproblems
        return helper(s,dict,0,dp);
     }
        private boolean helper(String s,Set<String>dict,int start, Boolean[] dp){
         if(start==s.length()) return true;
-        if(dp[start]!=null) return dp[start];//means it has been calculated
+        if(dp[start]!=null) return dp[start];//means it has been calculated,reuse this result
         for(int end=start+1;end<=s.length();end++){
             String part=s.substring(start,end);
             if(dict.contains(part)){
                 if(helper(s,dict,end,dp)){
-                    return dp[start]=true; //mark(store) start =true if start found
+                    return dp[start]=true; //mark start as true since valid partition found
                 }
             }
         }
-        return dp[start]=false; //mark(store) the result of dp[start]
+        return dp[start]=false; //mark start as false since no valid partition found 
        }
 }
     
+// dp[start] depends on dp[end], so overlapping subproblems are avoided using memoization
