7c8ebe96fee9618f72f719ea898212d8fa6d2570
Bhawya2531 <bhawya055@gmail.com>
2026-05-09 12:12:38 +0530

    Time: 2 ms (10.85%) | Memory: 46.6 MB (78.06%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,55 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int originalcolor=image[sr][sc];
-        if(originalcolor==color) return image;
-        dfs(image,sr,sc,originalcolor,color);
-        return image;
 
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        int original = image[sr][sc];
+
+        if(original == color) {
+            return image;
+        }
+
+        int n = image.length;
+        int m = image[0].length;
+
+        bfs(sr, sc, image, original, color, n, m);
+
+        return image;
     }
 
-        private void dfs(int[][]image,int r, int c,int originalcolor, int color){
-            if(r<0||c<0||r>=image.length||c>=image[0].length) return;
-            if(image[r][c]!=originalcolor) return;
-            image[r][c]=color;
+    public void bfs(int row, int col, int[][] image,
+                    int original, int color,
+                    int n, int m) {
 
-            dfs(image,r+1,c,originalcolor,color);
-            dfs(image,r-1,c,originalcolor,color);
-            dfs(image,r,c+1,originalcolor,color);
-            dfs(image,r,c-1,originalcolor,color);
+        Queue<int[]> q = new LinkedList<>();
+
+        q.offer(new int[]{row, col});
+
+        image[row][col] = color;
+
+        int[] dirrow = {-1, 0, 1, 0};
+        int[] dircol = {0, -1, 0, 1};
+
+        while(!q.isEmpty()) {
+
+            int[] curr = q.poll();
+
+            int r = curr[0];
+            int c = curr[1];
+
+            for(int i = 0; i < 4; i++) {
+
+                int nr = r + dirrow[i];
+                int nc = c + dircol[i];
+
+                if(nr >= 0 && nr < n &&
+                   nc >= 0 && nc < m &&
+                   image[nr][nc] == original) {
+
+                    image[nr][nc] = color;
+
+                    q.offer(new int[]{nr, nc});
+                }
+            }
         }
+    }
 }
-
-            
