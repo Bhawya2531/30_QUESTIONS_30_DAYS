07ad47f65a129fbc5ccbfb6ca31691fff872784a
Bhawya2531 <bhawya055@gmail.com>
2026-05-29 09:21:09 +0530

    Time: 16 ms (34.83%) | Memory: 46.2 MB (29.02%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,23 +1,35 @@
-class Solution {
+
+       class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
-       boolean[][]dp=new boolean[n][n];
-       int count=0;
-       for(int L=1;L<=n;L++){
-        for(int i=0;i<=n-L;i++){
-            int j=i+L-1;
-            if(s.charAt(i)==s.charAt(j)){
-                if(L<=2){
-                    dp[i][j]=true;
-                }else{
-                    dp[i][j]=dp[i+1][j-1];
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = i; j < n; j++) {
+
+                // first and last character must match
+                if (s.charAt(i) == s.charAt(j)) {
+
+                    // length 1 or 2
+                    if (j - i <= 1) {
+                        dp[i][j] = true;
+                    }
+
+                    // length > 2
+                    else if (dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                    }
                 }
-                if(dp[i][j]) count++;
+
+                if (dp[i][j]) {
+                    count++;
+                }
             }
         }
-       }
-       return count;
+
+        return count;
     }
 }
-
-               
