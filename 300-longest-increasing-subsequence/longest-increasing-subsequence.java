a32e323866a55c607e6c47435bc672db0542ce2e
Bhawya2531 <bhawya055@gmail.com>
2026-05-31 12:44:37 +0530

    Time: 84 ms (21.82%) | Memory: 237.4 MB (6.93%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        Integer[][]dp=new Integer[n+1][n+1];
-        return solve(nums,0,-1,dp);
-    }
-        
-        private int solve(int[]nums,int i, int prev,Integer[][]dp){
-           
-            if(i>=nums.length){ return 0;
-        }
-         if(dp[i][prev+1]!=null) {
-                return dp[i][prev+1];
+       int n=nums.length;
+       int[][]dp=new int[n+1][n+1];
+       for(int i=n-1;i>=0;i--){
+        for(int prev=i-1;prev>=-1;prev--){ //since prev is the last element it must be less than i 
+            int skip= dp[i+1][prev+1]; //since prev starts from -1 nd array cant store negative soshiftcol
+            int take=0;
+            if(prev==-1||nums[prev]<nums[i]){
+                take=1+dp[i+1][i+1];//prev value is i here and since we shifted col for prev i->i+1
             }
-        int take=0;
-        if(prev==-1||nums[prev]<nums[i]){
-        take=1+solve(nums,i+1,i,dp);
-        }
-        int skip=solve(nums,i+1,prev,dp);
-        return dp[i][prev+1]=Math.max(take,skip);
+       
+        dp[i][prev+1]=Math.max(take,skip); //  take skip correspond to this cell in recursion 
     }
 }
+return dp[0][0];    //cz recursion started with 0,-1 
+}
+}
+
