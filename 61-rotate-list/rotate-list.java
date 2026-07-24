f3d575af0bc0395b5fdfe8e7318be977edb5672d
Bhawya Kumari <bhawya055@gmail.com>
2026-07-24 11:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (26.24%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,32 +10,26 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-               if (head == null || head.next == null || k == 0) return head;
-
-        // Count the length
-        ListNode temp = head;
-        int length = 1;
-        while (temp.next != null) {
-            temp = temp.next;
-            length++;
+        if (head == null || head.next == null || k == 0) return head;
+        int L=1;
+        ListNode tail=head;
+        while(tail.next!=null){
+            tail=tail.next;
+            L++;
         }
-
-        // Make it circular
-        temp.next = head;
-
-        // Find new tail: (length - k % length - 1)
-        // and new head: (length - k % length)
-        k = k % length;
-        int stepsToNewHead = length - k;
-        ListNode newTail = temp;
-        while (stepsToNewHead-- > 0) {
-            newTail = newTail.next;
+        k=k%L;
+        if(k==0){
+            return head;
         }
-
-        // Break the loop
-        ListNode newHead = newTail.next;
-        newTail.next = null;
-
-        return newHead;
+        tail.next=head; //circular connect (tail will now point to head cuz there gonna be a new head)
+         //L-K
+         int remain=L-k;
+         ListNode newTail=head;
+         for(int c=1;c<remain;c++){
+            newTail=newTail.next;
+         }
+         ListNode newhead=newTail.next;
+         newTail.next=null;
+         return newhead;
     }
 }
