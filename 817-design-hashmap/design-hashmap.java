61bcc1db5a56d19c0f17733ad35cdef9263ebcaf
Bhawya Kumari <bhawya055@gmail.com>
2026-08-12 02:03:44 +0530

    Time: 14 ms (99.74%) | Memory: 48.2 MB (90.86%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.java
@@ -0,0 +1,73 @@
+class MyHashMap {
+    class Node{
+        int key;
+        int value;
+        Node next;
+    Node(int key, int value){
+        this.key=key;
+        this.value=value;
+    }
+}
+Node[]table;
+int size=1000;
+    public MyHashMap() {
+        table=new Node[size];
+    }
+    public void put(int key, int value) {
+        int index=key%size;
+        if(table[index]==null){
+            table[index]=new Node(key,value);
+            return;
+        }
+        Node curr=table[index];
+        while(curr!=null){
+            if(curr.key==key){
+                curr.value=value;
+                return;
+            }
+            if(curr.next==null){
+                break;
+            }
+            curr=curr.next;
+        }
+        curr.next=new Node(key,value);
+    }
+    
+    public int get(int key) {
+        int index=key%size;
+        Node curr=table[index];
+        while(curr!=null){
+            if(curr.key==key){
+                return curr.value;
+            }
+            curr=curr.next;
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int index=key%size;
+        Node curr=table[index];
+        Node prev=null;
+        while(curr!=null){
+            if(curr.key==key){
+                if(prev==null){
+                    table[index]=curr.next;
+                }else{
+                    prev.next=curr.next;
+                }
+                return;
+            }
+            prev=curr;
+            curr=curr.next;
+        }
+    }
+    }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
